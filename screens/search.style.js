4744a11c6ae1f5732054c582c7fef0f60a70001c
Muhammad Mubashar Khan <mubasher9170@gmail.com>
2024-01-12 17:16:16 +0500

    Add Search bar View in search screen

--- screens/search.style.js
+++ screens/search.style.js
@@ -2,16 +2,6 @@ import { StyleSheet } from "react-native";
 import { COLORS, SIZES} from "../constants/index";
 
 const styles = StyleSheet.create({
-    container: {
-        width: "100%",
-    },
-    welcomeTxt: (color, top) => ({
-        fontFamily: "bold",
-        fontSize: SIZES.xxLarge - SIZES.xSmall,
-        marginTop: top,
-        color: color,
-        marginHorizontal: SIZES.small,
-    }),
     searchContainer: {
         flexDirection: "row",
         justifyContent: "center",
